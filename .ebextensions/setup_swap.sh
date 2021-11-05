3abb57999bb620b4cce6c8f7fff75d296c03deb9
LennyLip <vermus.jabber@gmail.com>
2021-11-05 22:47:13 +0300

    fix swap

--- .ebextensions/setup_swap.sh
+++ .ebextensions/setup_swap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 SWAPFILE=/var/swapfile
-SWAP_MEGABYTES=1534
+SWAP_MEGABYTES=1200
 
 if [ -f $SWAPFILE ]; then
 	echo "Swapfile $SWAPFILE found, assuming already setup"
+	/sbin/swapon $SWAPFILE
 	exit;
 fi
 
